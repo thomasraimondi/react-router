2ad1b961c36bf36d698c565b4902ca58936c5283
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-30 13:40:43 +0200

    add style to Header Components

--- src/assets/components/layout/default/Header.jsx
+++ src/assets/components/layout/default/Header.jsx
@@ -1,3 +1,28 @@
+import { NavLink } from "react-router-dom";
+
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <nav className="flex gap-4 items-center border-b border-gray-200 shadow-sm p-4">
+      <a className="navbar-brand" href="/">
+        <img className="h-10" src="https://www.comparacorsi.it/wp-content/uploads/2023/07/boolean.png" alt="Logo" height="30" />
+      </a>
+      <ul className="flex gap-4">
+        <li>
+          <NavLink to="/" className={({ isActive }) => (isActive ? "text-blue-500 font-bold" : "")}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className={({ isActive }) => (isActive ? "text-blue-500 font-bold" : "")}>
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/posts" className={({ isActive }) => (isActive ? "text-blue-500 font-bold" : "")}>
+            Posts
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
