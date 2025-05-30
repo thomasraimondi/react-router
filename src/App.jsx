68c6e20020b7198b01d2340cdb566a2c60153323
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-30 13:14:18 +0200

    install react router and add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./assets/pages/HomePage";
+
 function App() {
   return (
-    <>
-      <h1>My app</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
