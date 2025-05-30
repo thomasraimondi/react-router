8e5533f4982da432e51e07150ac38fcde50e0612
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-30 16:24:28 +0200

    delete p in footer

--- src/components/layout/default/Footer.jsx
+++ src/components/layout/default/Footer.jsx
@@ -1,7 +1,3 @@
 export default function Footer() {
-  return (
-    <footer className="flex justify-center items-center border-t bg-black text-white opacity-80 shadow-sm p-4 h-30">
-      <p className="text-gray-500">Footer</p>
-    </footer>
-  );
+  return <footer className="flex justify-center items-center border-t bg-black text-white opacity-80 shadow-sm p-4 h-30"></footer>;
 }
