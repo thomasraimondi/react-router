18aa7d338d05de01efc1f3d24ca77f8581eb5b84
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-30 14:04:21 +0200

    add content in Abount pages

--- src/assets/pages/About.jsx
+++ src/assets/pages/About.jsx
@@ -1,3 +1,18 @@
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <>
+      <div className="relative jumbotron bg-blue-500 text-white h-30">
+        <img src="https://img.freepik.com/free-vector/gradient-minimalist-background_23-2149974334.jpg" alt="Logo" className="w-full h-full object-cover" />
+        <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-2xl font-bold">About</span>
+      </div>
+      <section className="my-5 container mx-auto flex flex-col gap-4">
+        <h2 className="text-2xl font-bold">Cosa puoi fare su Thomas's Blog?</h2>
+        <p>
+          Benvenuto nel sito dei post! Qui puoi leggere, creare e condividere i tuoi pensieri con gli altri utenti. Esplora i post pubblicati dalla nostra community, lasciati ispirare dalle idee degli
+          altri e partecipa anche tu condividendo le tue opinioni. Registrati o accedi per iniziare a scrivere, commentare e interagire con gli altri membri. Il nostro obiettivo è creare uno spazio
+          aperto dove tutti possano esprimersi liberamente e trovare nuovi spunti ogni giorno. Buona navigazione!
+        </p>
+      </section>
+    </>
+  );
 }
