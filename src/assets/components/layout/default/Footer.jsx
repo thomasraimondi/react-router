b2a83fe92d5a3a3e69587da547fb900d0566e95d
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-30 13:59:24 +0200

    add style in footer component

--- src/assets/components/layout/default/Footer.jsx
+++ src/assets/components/layout/default/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center border-t border-gray-200 shadow-sm p-4">
+    <footer className="flex justify-center items-center border-t bg-black text-white opacity-80 shadow-sm p-4 h-30">
       <p className="text-gray-500">Footer</p>
     </footer>
   );
